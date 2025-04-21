0d24214aa238e75d3f733af591efe8de48d7fdd2
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-04-21 23:40:37 +0300

    Add new navigation links and update Login link

--- components/NavigatonMenu.tsx
+++ components/NavigatonMenu.tsx
@@ -4,19 +4,26 @@ const NavigationMenu = () => {
   return (
     <nav>
       <ul className="flex gap-4 p-4 bg-gray-100">
+        <>
+          <li>
+            <Link href="/" className="text-blue-600 hover:underline">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/users" className="text-blue-600 hover:underline">
+              Users
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="text-blue-600 hover:underline">
+              About
+            </Link>
+          </li>
+        </>
         <li>
-          <Link href="/" className="text-blue-600 hover:underline">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/users" className="text-blue-600 hover:underline">
-            Users
-          </Link>
-        </li>
-        <li>
-          <Link href="/about" className="text-blue-600 hover:underline">
-            About
+          <Link href="/login" className="text-blue-600 hover:underline">
+            Login
           </Link>
         </li>
       </ul>
