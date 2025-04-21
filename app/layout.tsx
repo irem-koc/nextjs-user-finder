aa526722db3a14e8814c5409817bb536f7711506
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-04-21 23:26:18 +0300

    Add SessionWrapper component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import NavigationMenu from "@/components/NavigatonMenu";
+import SessionWrapper from "@/components/SessionWrapper";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,8 +29,10 @@ export default function RootLayout({
         suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NavigationMenu />
-        {children}
+        <SessionWrapper>
+          <NavigationMenu />
+          {children}
+        </SessionWrapper>
       </body>
     </html>
   );
